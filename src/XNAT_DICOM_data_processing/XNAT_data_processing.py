f4e46f68cb7e29f2af994892c0dc8fe7b687e9fd
eliseleahy <elisepather@gmail.com>
2024-06-22 19:42:03 +0100

    loop to change tags

--- src/XNAT_DICOM_data_processing/XNAT_data_processing.py
+++ src/XNAT_DICOM_data_processing/XNAT_data_processing.py
@@ -14,40 +14,47 @@ def remove_tag_val(tag_num:list) -> None:
 def change_study_desc() -> None:
     dataset[0x0008, 0x1030].value = 'This is a test study description'
     
+def reconfigure_subject_session(folder_dir:str) -> str:
+    folder_dir.split('GSTT000030')
+    
 
 if __name__=='__main__':
+    # connect to xnat testproject using session
     with xnat.connect('http://localhost',user='admin', password='admin') as session:
         testproject=session.projects['testproject']
-        print(testproject)
-        print(testproject.subjects)
+        #select subject
         subject = testproject.subjects['GSTT000030']
-        subject.download_dir('./')
+        #download subject directory
+        subject.download_dir('/mnt/c/Repos/196-LIS8437-EP/dev-data/DICOM_temp/')
 
-        r = []
-        dir = '/mnt/c/Repos/196-LIS8437-EP/GSTT000030/'
+        # loop over directory files to change each dicom file
+        dir = '/mnt/c/Repos/196-LIS8437-EP/dev-data/DICOM_temp/GSTT000030/'
         for root, dirs, files in os.walk(dir):
             for name in files:
-                r.append(os.path.join(root, name))
+                
                 file = os.path.join(root, name)
                 dataset = pydicom.dcmread(file)
-                print(file)
-                print(dataset[0x0008, 0x0020], dataset[0x0008, 0x0022], dataset[0x0008, 0x0012])
                 add_days_to_tags([[0x0008, 0x0020], [0x0008, 0x0022], [0x0008, 0x0012]], 11)
-                print(dataset[0x0008, 0x0020], dataset[0x0008, 0x0022], dataset[0x0008, 0x0012])
-
-
-
-                print(dataset[0x0008, 0x0030], dataset[0x0008, 0x0032], dataset[0x0008, 0x0013])
                 remove_tag_val([[0x0008, 0x0030], [0x0008, 0x0032], [0x0008, 0x0013]])
-                print(dataset[0x0008, 0x0030], dataset[0x0008, 0x0032], dataset[0x0008, 0x0013])
-
-
-                
-                print(dataset[0x0008, 0x1030].value)
                 change_study_desc()
-                print(dataset[0x0008, 0x1030].value)
 
 
+                #create new session folder and save edited dicom files
+                path_split = file.split('69c22ac9-0343-4c32-8b4b-5941883f3177')
+                print(file)
+                new_file = path_split[0] + 'SessionA' + path_split[1]
+                filepath = new_file.rsplit('/', 1)[0]
+                if not os.path.exists(filepath):
+                    os.makedirs(filepath)
+                os.chdir(filepath)
+                dataset.save_as(new_file)
+                print(new_file)
+                
+        # next step is to push the new session to XNAT
+        
+
+
+            
 
 
 
